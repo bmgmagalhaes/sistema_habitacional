9b6003438243214930a4f48387ce84d5bacf5f58
Bruno Magalhães Gonçalves <bm.goncalves@gmail.com>
2026-06-23 09:38:19 -0300

    Ajusta configurações CSRF para produção

--- project/settings.py
+++ project/settings.py
@@ -153,3 +153,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-6c4b.up.railway.app",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
